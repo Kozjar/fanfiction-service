85584bf458a7cce76004e531e56e11829914e877
Kozjar <qwertzxc0000@gmail.com>
2019-12-16 20:42:25 +0300

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongodb: {
+        // dbURL: 'mongodb+srv://kk:Stroburus1@cluster0-six0s.mongodb.net/test?retryWrites=true&w=majority'  // mobile 46.216.152.129 ip
+        dbURL: 'mongodb+srv://Ankirig:Stroburus1@cluster0-y6thq.mongodb.net/test?retryWrites=true&w=majority'
+    }
+}
